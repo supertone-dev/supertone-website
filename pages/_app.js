41fe813adfb1ee1dc92be72cb35024f5d4fcda03
Jack Damon <jdamon96@gmail.com>
2021-04-13 16:57:37 -0400

    implemented responsive mobile design

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,10 @@ function MyApp({ Component, pageProps }) {
           href="https://use.typekit.net/gsd5aql.css"
         ></link>
         <link rel="icon" href="/favicon.svg" />
+        <meta
+          name="viewport"
+          content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"
+        />
       </Head>
       <Component {...pageProps} />
     </>
--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,11 @@ export default function Home() {
   return (
     <>
       <div className={`flex flex-col w-screen ${formIsOpen ? "hidden" : ""}`}>
-        <header className="w-full h-16 flex justify-center items-center">
+        <header className="w-full h-16 flex justify-center items-center mb-8">
           <div className="w-3/4 flex justify-between items-center">
-            <div className="text-4xl font-futuraBold font-bold">Supertone</div>
+            <div className="text-2xl md:text-4xl font-futuraBold font-bold">
+              Supertone
+            </div>
             {/* <button className="bg-gray-800 rounded-md h-9 text-white px-4 text-center align-middle">
             Get early access
           </button> */}
@@ -26,21 +28,21 @@ export default function Home() {
         </header>
         <main className="h-full w-full flex flex-col justify-center items-center">
           <div className="w-1/2 text-center">
-            <p className="text-6xl font-minion font-bold text-gray-900 my-4">
+            <p className="text-4xl md:text-6xl font-minion font-bold text-gray-900 my-4">
               Easily organize and access all your samples.
             </p>
-            <p className="text-xl text-gray-700 my-4">
+            <p className="text-lg md:text-xl text-gray-700 my-4">
               Supertone is tool for digital musicians. Quickly tag, search, and
               access all your samples through a single interface.
             </p>
           </div>
           <button
             onClick={onClick}
-            className="bg-gradient-to-r from-gray-700 via-gray-600 to-gray-800 text-white rounded-md px-4 py-2 my-5"
+            className="bg-gray-900 text-white rounded-md px-4 py-2 my-5"
           >
             Get early access
           </button>
-          <div className="flex">
+          <div className="flex flex-col md:flex-row">
             <img src="/samples.png" width="400px" className="rounded-md" />
             <img src="/tags.png" width="400px" className="rounded-md" />
           </div>
